11b1f50e7accff305d86423b63803a9c459e738b
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-07-26 01:15:06 +0530

    server created

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.json())
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
+app.get('/home',(req,res)=>{
+   res.send("Hello")});
